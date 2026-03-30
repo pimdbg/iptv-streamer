fd51bcfccb405d836b7f1f72e5b299ce4bdd8475
Pim <84320205+pimdbg@users.noreply.github.com>
2026-03-30 10:38:22 +0200

    Update Routes.ts

--- renderer/src/routing/Routes.ts
+++ renderer/src/routing/Routes.ts
@@ -1,9 +1,11 @@
 import WelcomePage from "../pages/WelcomePage";
 import ChannelListPage from "../pages/ChannelsListPage";
+import LiveTvStreamer from "../pages/LiveTvStreamer";
 
 const Routes: Record<string, React.FC> = {
     "/": WelcomePage,
     "/channels-list": ChannelListPage,
+    "/live-tv": LiveTvStreamer
 }
 
 export default Routes;
