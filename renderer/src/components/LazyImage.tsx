9b17bf260a0da0540d66d170038215667c34fea0
Pim <84320205+pimdbg@users.noreply.github.com>
2026-03-30 10:37:29 +0200

    Create LazyImage.tsx

---
+++ renderer/src/components/LazyImage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, useState } from "react";
+import { cx } from "../utils/cctx";
+
+export function LazyImage({ src, className, ...props }: React.ImgHTMLAttributes<HTMLImageElement>) {
+    const ref = useRef<HTMLImageElement>(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useIntersectionObserver(ref, (entry) => {
+        if (entry.isIntersecting) {
+            setIsLoaded(true);
+        }
+    }, { threshold: 0.1 });
+    
+    return (
+        <img 
+            src={isLoaded ? src : undefined}
+            className={cx("object-contain", className)}
+            ref={ref}
+            {...props}
+        />
+    )
+}
+
+function useIntersectionObserver<T extends HTMLElement | null = HTMLElement>(
+    ref: React.RefObject<T>,
+    callback: (entry: IntersectionObserverEntry) => void,
+    options?: IntersectionObserverInit
+) {
+    useEffect(() => {
+        if (!ref.current) return;
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                callback(entry);
+            });
+        }, options);
+
+        observer.observe(ref.current);
+
+        return () => {
+            observer.disconnect();
+        }
+    }, [ref, callback, options]);
+}
