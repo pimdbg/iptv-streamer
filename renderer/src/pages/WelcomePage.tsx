df2c3c0d215c6dcc1d9b4df8115ec579ef3c3dca
Pim <84320205+pimdbg@users.noreply.github.com>
2026-03-30 10:34:13 +0200

    Update WelcomePage background

--- renderer/src/pages/WelcomePage.tsx
+++ renderer/src/pages/WelcomePage.tsx
@@ -1,31 +1,50 @@
 import { PageCard } from "@/components/PageCard";
-import { goTo } from "@/routing/utils";
+import { goTo } from "@/routing/utils"; 
+import TvRetroIcon from "@/assets/icons/tv-retro.svg";
+import { Icon } from "@/components/Icon";
 
 const WelcomePage = () => {
   return (
-    <div className="bg-linear-to-tr from-[rgb(42,39,55)] to-[rgb(76,71,106)] w-screen h-screen p-10">
-      <div className="flex flex-col h-full">
-        <h1 className="text-4xl text-white font-light">Welcome</h1>
-        <div className="flex-1 flex">
-          <div className="w-full h-92 self-center flex gap-6">
-            <PageCard 
-              title="Live TV"
-              subTitle="Subtitle" 
-              className="backdrop-blur-xl"
-              onClick={() => {
-                goTo("#/channels-list");
-              }}
-            />
-            <PageCard 
-              title="Movies"
-              subTitle="Subtitle" 
-              onClick={() => alert('Not Active')}
-            />
-            <PageCard 
-              title="Radio"
-              subTitle="Subtitle" 
-              onClick={() => alert('Not Active')}
-            />
+    <div className="w-screen h-screen bg-bg" style={{
+
+      backgroundImage: `
+        linear-gradient(to right, var(--color-bg), 5%, var(--color-bg), 80%, rgba(76,71,106,0.85)),
+        url(https://images.unsplash.com/photo-1554629947-334ff61d85dc?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)
+      `,
+      backgroundSize: "cover, 80% cover",
+      backgroundPosition: "center, right",
+      backgroundRepeat: "no-repeat, no-repeat",
+    }}>
+      <div className="w-full h-full relative top-0 p-10 z-10">
+        <div className="flex flex-col h-full">
+          <h1 className="text-4xl text-white font-light">Welcome</h1>
+          <div className="flex-1 flex">
+            <div className="w-full h-92 self-center flex gap-6">
+              <PageCard 
+                title="Live TV"
+                subTitle="Subtitle" 
+                icon={
+                  <Icon 
+                    icon={TvRetroIcon} 
+                    size={48} 
+                  />
+                }
+                className="backdrop-blur-xl"
+                onClick={() => {
+                  goTo("#/channels-list");
+                }}
+              />
+              <PageCard 
+                title="Movies"
+                subTitle="Subtitle" 
+                onClick={() => alert('Not Active')}
+              />
+              <PageCard 
+                title="Radio"
+                subTitle="Subtitle" 
+                onClick={() => alert('Not Active')}
+              />
+            </div>
           </div>
         </div>
       </div>
